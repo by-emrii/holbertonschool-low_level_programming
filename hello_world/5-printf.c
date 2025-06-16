c47401a463a12d8388b72acf96f876ee315d38ea
Emily Chew <emilychewsh@gmail.com>
2025-06-16 07:29:50 +0000

    printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: puts func to print a string
+ * Return: 0 meaning success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
