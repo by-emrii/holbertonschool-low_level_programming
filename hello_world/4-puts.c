0f3dd0d148d991f67a9ad6ed1e9c3d369f4b7bb3
Emily Chew <emilychewsh@gmail.com>
2025-06-16 07:24:11 +0000

    finally

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - is the entry point of func with no arg entry
+ * Description: puts func to print a string
+ * Return: 0 meaning success
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
