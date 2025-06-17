4461b487e39d85ed0e982ce651fa9712dbff2222
Emily Chew <emilychewsh@gmail.com>
2025-06-17 03:19:44 +0000

    elif

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: if statements
+ * Return: Success (0)
+ */
+int main(void)
+{
+	int n;
+	int last_d;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last_d = n % 10;
+
+	if (last_d > 5)
+	{
+		printf("Last digit of %d is %d and is greter than 5\n", n, last_d);
+	}
+	else if (last_d == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, last_d);
+	}
+	else if (last_d < 6 && last_d != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_d);
+	}
+	return (0);
+}
