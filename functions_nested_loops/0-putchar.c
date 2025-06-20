0e7a6ec8d26542f012d49b4d9b9b28bf63b25b75
Emily Chew <emilychewsh@gmail.com>
2025-06-20 01:18:50 +0000

    print _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: print _putchar
+ * Return: Success (0)
+ */
+int main(void)
+{
+	puts("_putchar");
+	return (0);
+}
---
+++ functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+C - functions and nested loops projects for holberton week 3
