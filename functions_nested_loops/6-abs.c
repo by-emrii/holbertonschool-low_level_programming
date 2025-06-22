5256b507164309a39f6b3eb47a1734432a0ecb36
Emily Chew <emilychewsh@gmail.com>
2025-06-22 09:36:29 +0000

    task 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - computes abs value of an integer
+ * @n: input
+ * Return: abs value of n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+}
