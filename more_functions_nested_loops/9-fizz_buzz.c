b0c83d0cc995881aeee98206ea3fc2929d2d035a
Emily Chew <emilychewsh@gmail.com>
2025-06-23 07:17:34 +0000

    task 9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * fizzbuzz - Prints numbers from 1 to 100 with FizzBuzz logic
+ * Return: void
+ */
+void fizzbuzz(void)
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+		putchar(' ');
+		i++;
+	}
+	putchar('\n');
+}
+/**
+ * main - entry point for the program
+ * Return: 0 on success
+ */
+int main(void)
+{
+	fizzbuzz();
+	return (0);
+}
