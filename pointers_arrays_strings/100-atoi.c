06ab83d6dbbf823979ce39dcd18974006b1acbe3
Emily Chew <emilychewsh@gmail.com>
2025-06-30 02:10:01 +0000

    task 10

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _atoi - convert a string to an integer
+ * @s: pointer to a char
+ * Return: int = num * sign
+ */
+int _atoi(char *s)
+{
+	int current_index = 0;
+	int sign = 1;
+	int number = 0;/*builds and stores the actual int value*/
+	int number_started = 0; /*flag: becomes true (1) when it encounters digits*/
+
+	while (s[current_index] != '\0')
+	{
+		if (s[current_index] == '-' && !number_started)
+		{
+			sign = sign * -1;
+		}
+		/*if current num is a digit*/
+		else if (s[current_index] >= '0' && s[current_index] <= '9')
+		{
+			number_started = 1;/*set flag to true=number has started*/
+			number = number * 10 + (s[current_index] - '0');
+		}
+		else if (number_started)
+		{
+			break;
+		}
+		current_index++;
+	}
+	return (number * sign);
+}
