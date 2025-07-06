3a4e90a7ff56922cd305b636ebc477e8e82516cf
Emily Chew <emilychewsh@gmail.com>
2025-07-06 02:21:55 +0000

    task 2

---
+++ argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints all arguments passed
+ * @argc: argument count
+ * @argv: pointer to arg array
+ * Return: void
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
