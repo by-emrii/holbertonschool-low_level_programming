e13d07be9baf4cbe6ce2c9b282a6ed77124c47fb
Emily Chew <emilychewsh@gmail.com>
2025-07-08 07:05:54 +0000

    tbc

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * 
+ * @
+ * Return: void
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	unsigned int total_size;
+	
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	total_size = nmemb * size;
+
+	ptr = malloc(total_size);
+	if (ptr == NULL)
+		return (NULL);
+}
