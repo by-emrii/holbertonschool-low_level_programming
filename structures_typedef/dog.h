91f7088a016f41c5d83394935d9bf09c5f912451
Emily Chew <emilychewsh@gmail.com>
2025-07-10 06:02:50 +0000

    task 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog
+ * @name: pointer to dog name
+ * @age: age of dog
+ * @owner: pointer to dog owner
+ * Description - struct for dog
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
