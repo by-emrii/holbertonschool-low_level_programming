245cdd75954effa18e31889a179f8422205e20e3
Emily Chew <emilychewsh@gmail.com>
2025-07-15 02:37:48 +0000

    task 3

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "calc.h"
+#include <stdlib.h>
+/**
+ * op_add - add a and b
+ * @a: num1
+ * @b: num2
+ * Return: int
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - sub a and b
+ * @a: num1
+ * @b: num2
+ * Return: int
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - mul a and b
+ * @a: num1
+ * @b: num2
+ * Return: int
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - div a and b
+ * @a: num1
+ * @b: num2
+ * Return: int
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - mod a and b
+ * @a: num1
+ * @b: num 2
+ * Return: int
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
