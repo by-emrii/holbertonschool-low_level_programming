73a9619ba6598d9e1268beb2dc29a3466a2e60e5
Emily Chew <emilychewsh@gmail.com>
2025-07-17 04:31:39 +0000

    task 3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,35 +2,101 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <stdio.h>
+
+/* helper functions */
 /**
- * 
- * @
+ * print_char - prints char
+ * @args: va_list containing the correct arg
  * Return: void
  */
 
-void print_c()
+void print_char(va_list args)
 {
+	char c = (char)va_arg(args, int); /* get next arg as int, cast to char */
+
+	printf("%c", c);
 }
+
+/**
+ * print_int - prints int
+ * @args: va_list containing the correct arg
+ * Return: void
+ */
+
+void print_int(va_list args)
+{
+	int i = va_arg(args, int);
+
+	printf("%d", i);
+}
+
+/**
+ * print_float - prints float
+ * @args: va_list containing the correct arg
+ * Return: void
+ */
+
+void print_float(va_list args)
+{
+	float f = (float)va_arg(args, double);
+
+	printf("%f", f);
+}
+
+/**
+ * print_str - prints str
+ * @args: va_list containing the correct arg
+ * Return: void
+ */
+
+void print_str(va_list args)
+{
+	char *s = va_arg(args, char *);
+
+	if (s == NULL)
+		printf("(nil)");
+	else
+		printf("%s", s);
+}
+
+/**
+ * print_all - function that prints anything based on format str
+ * @format: types of arg passed
+ * Return: void
+ */
 
 void print_all(const char * const format, ...)
 {
-	va_list args;
-	char *separator;
-	int i = 0;
-	int flag = 0;
-	char *str;
-	int int_val;
-	char char_val;
-	double float_val;
+	va_list args; /* access variable args */
+	char *separator = ""; /* start w empty sep, no comma b4 first val*/
+	int i = 0, j;
 
-	va_start(args, format);
-	separator = "";
+	/* array of structs that matches type chars to print funcs */
+	printer_t types[] = {
+		{'c', print_char},
+		{'i', print_int},
+		{'f', print_float},
+		{'s', print_str}
+	};
+
+	va_start(args, format); /* init va_list to process args */
 
 	while (format[i] != '\0')
 	{
-		if (format[i] == "c" || format[i] == "i" || format[i] == "f" || format[i] == "s")
+		j = 0;
+		while (j < 4)
 		{
-			printf("%s", separator);
+			if (format[i] == types[j].type)
+			{
+				printf("%s", separator);
+				types[j].f(args); /* call the correct print func */
+				separator = ", ";
+				break; /* found the correct type, break inner loop */
+			}
+			j++;
 		}
+		i++;
 	}
+	va_end(args); /* clean va_list */
+	printf("\n");
 }
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,6 +1,18 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+#include <stdarg.h>
+/**
+ * struct printer - struct for matching forma type with a func
+ * @type: format specifier e.g. 'c', 'i'...
+ * @f: func to print correct type
+ */
+typedef struct printer
+{
+	char type;
+	void (*f)(va_list args);
+} printer_t;
+
 /*prototypes for my funcs and _putchar*/
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
