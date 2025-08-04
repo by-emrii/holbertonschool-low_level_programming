234b0daa36253216e86520e7a943622ee71dd984
Emily Chew <emilychewsh@gmail.com>
2025-08-04 08:36:55 +0000

    task 3

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,49 @@
+#include "hash_tables.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * hash_table_set - adds element to the hash table
+ * @ht: the hash table you want to add/update to 
+ * @key: the key. cannot be empty string
+ * @value: value assoc with the key, must be dup, can be empty str
+ * Return: 1 if succeeded, 0 if fail
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *node;
+	
+	/* if node does not exist, create node, for later*/
+        hash_node_t *new_node = malloc(sizeof(hash_node_t));
+        if (!new_node)
+                return (0);
+
+	if (!ht || !key || !value)
+		return (0);
+	
+	/* turn key into index using func*/
+	index = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[index]; /*looks inside ht for node*/
+
+	/*update value if key exists*/
+	while (node)
+	{
+		if (strcmp(node->key, key) == 0) /*compare the two key str*/
+		{
+			free(node->value);
+			node->value = strdup(value);
+			return (1);
+		}
+		node = node->next; /* keep searching */
+	}
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+
+	new_node->next = ht->array[index]; /*insert it at head*/
+	ht->array[index] = new_node;
+
+	return (1);
+}
