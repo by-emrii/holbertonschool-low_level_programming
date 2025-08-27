fcdea754f832ca23f6a55bed2451bdd4673bb2ea
Emily Chew <emilychewsh@gmail.com>
2025-08-27 05:26:43 +0000

    betty

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 
 /**
- * function that appends text at the end of a file.
+ * append_text_to_file - function that appends text at the end of a file.
  * @filename: name of the file to create
  * @text_content: a NULL terminated string to write to the file
  * Return: 1 on success and -1 on failure
