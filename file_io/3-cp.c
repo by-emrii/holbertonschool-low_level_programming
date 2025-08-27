a59d5f2bc7c878191a53a8bbaca28d9a0a331a30
Emily Chew <emilychewsh@gmail.com>
2025-08-27 08:33:19 +0000

    revert

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -3,8 +3,6 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "check_args.c"
-#include "open_helpers.c"
 
 /**
  * main - entry point
@@ -12,34 +10,39 @@
  * @argv: ptr to argument str
  * Return: void
  */
-
 int main(int argc, char *argv[])
 {
 	int fd_from, fd_to;
+	char *file_from, *file_to;
 	char buf[1024];
 	ssize_t bytes_read, bytes_written;
 
-	check_args(argc);
-	fd_from = open_source(argv[1]);
-	fd_to = open_dest(argv[2]);
-
+	if (argc != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	file_from = argv[1];
+	file_to = argv[2];
+	/* open source for reading */
+	fd_from = open(file_from, O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+	/* open or create dest file for writing */
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to), exit(99);
 	/* read from source and write to dest in 1024 bytes */
 	while ((bytes_read = read(fd_from, buf, 1024)) > 0)
 	{
 		bytes_written = write(fd_to, buf, bytes_read);
 		if (bytes_written != bytes_read || bytes_written == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close(fd_from);
-			close(fd_to);
-			exit(99);
-		}
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to), exit(99);
 	}
+	/* check read error */
 	if (bytes_read == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		close(fd_from);
-		close(fd_to);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 		exit(98);
 	}
 	/* close file */
