eafadb4818b51855b9ef80901ef65ec150c27dbe
Emily Chew <emilychewsh@gmail.com>
2025-08-27 08:58:58 +0000

    fix

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -15,12 +15,11 @@ int main(int argc, char *argv[])
 	int fd_from, fd_to;
 	char *file_from, *file_to;
 	char buf[1024];
-	ssize_t bytes_read, bytes_written;
+	ssize_t bytes_read = 1024, bytes_written;
 
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	file_from = argv[1];
-	file_to = argv[2];
+	file_from = argv[1], file_to = argv[2];
 	/* open source for reading */
 	fd_from = open(file_from, O_RDONLY);
 	if (fd_from == -1)
@@ -33,17 +32,18 @@ int main(int argc, char *argv[])
 	if (fd_to == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to), exit(99);
 	/* read from source and write to dest in 1024 bytes */
-	while ((bytes_read = read(fd_from, buf, 1024)) > 0)
+	while (bytes_read == 1024)
 	{
+		bytes_read = read(fd_from, buf, 1024);
+		/* check read error */
+		if (bytes_read == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+			exit(98);
+		}
 		bytes_written = write(fd_to, buf, bytes_read);
-		if (bytes_written != bytes_read)
+		if (bytes_written < bytes_read)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to), exit(99);
-	}
-	/* check read error */
-	if (bytes_read == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-		exit(98);
 	}
 	/* close file */
 	if (close(fd_from) == -1)
